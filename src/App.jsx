0951777370fb61e32d9513c33f2df1ed08477589
omarakamal <omaraakamal@gmail.com>
2025-02-16 18:45:12 +0300

    initial upload

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+import {Routes, Route} from 'react-router'
+import AllPets from './pages/AllPets'
+import PetDetails from './pages/PetDetails'
+import CreatePet from './pages/CreatePet'
+import Navbar from './components/Navbar'
+import PageNotFound from './pages/PageNotFound'
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+     <Routes>
+      <Route path="/pets" element = {<AllPets/>}/>
+      <Route path="/pets/:id" element={<PetDetails/>}/>
+      <Route path="/pets/create" element={<CreatePet/>}/>
+      <Route path='*' element={<PageNotFound/>}/>
+     </Routes>
+    </>
+  )
+}
+
+export default App
